e2680fd274a2f0bbfe22ec21e673f66faacb84b5
Justin Goodrich <justingoodrich@Justins-Air.attlocal.net>
2023-01-23 07:10:15 -0500

    testing fix

--- tests/CameraControler_test.py
+++ tests/CameraControler_test.py
@@ -7,20 +7,21 @@ class TestCameraController:
     camera = CameraController(0)
 
     def test_is_working(self):
-        success = self.camera.camera.grab()
+        cam = CameraController(0)
+        success = cam.camera.grab()
         assert success == True
 
     def test_capture_frame(self):
-        image = self.camera.capture_frame()
+        cam = CameraController(0)
+        image = cam.capture_frame()
         assert isinstance(image, np.ndarray)
 
 class TestImageCamera:
-    image_camera = ImageCamera(0)
-
     def test_take_photos(self,tmp_path):
+        cam = ImageCamera(0)
         n_photos = 5
         increment = 1
         d = tmp_path / "sub"
-        self.image_camera.image_collection(increment,n_photos,d)
+        cam.image_collection(increment,n_photos,d)
         assert len(os.listdir(d)) == n_photos
 
