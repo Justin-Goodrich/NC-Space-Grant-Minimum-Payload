e5a1ae57ef42466f60d91a54af702b293c8b845b
Justin Goodrich <justingoodrich@Justins-Air.attlocal.net>
2023-02-05 22:17:23 -0500

    altimeter class bug fixes

--- Altimeter.py
+++ Altimeter.py
@@ -16,7 +16,7 @@ CTRL_REG2 = 0x27
 
 ACTIVE = 0x01
 MODE_ALTIMETER_OSR_128 = 0xB8
-MODE_ALTIMETER = 80
+MODE_ALTIMETER = 128
 
 ALTITUDE_DIVISOR = 65536
 PRESSURE_DIVISOR = 64
@@ -39,9 +39,17 @@ class Altimeter:
     def wipe(self):
         self.bus.write_byte_data(MPL3115A2_ADDR,CTRL_REG1,0x00)
     
+    def convert_altitude_data(self,data):
+        return float((data[0]<<24)|(data[1]<<16)|(data[2]<<8))/ALTITUDE_DIVISOR
+
+    def convert_pressure_data(self,data):
+        return float((data[0]<<16)|(data[1]<<8)|(data[2]))/PRESSURE_DIVISOR
+
+    def convert_temperature_data(self,data):
+        return float((data[0]<<8)|(data[1]))/TEMPERATURE_DIVISOR
 
     def get_altitude(self):
-
+        # checks if altitude mode is on, if off, switches back to altitude
         if not (self.config & MODE_ALTIMETER):
             self.configure(self.config | MODE_ALTIMETER)
 
@@ -52,17 +60,19 @@ class Altimeter:
         byte_data[2] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_LSB)
         
         return self.convert_altitude_data(byte_data)
+    
+    def get_barometric_pressure(self):
+        # checks if altitude mode is on, if on, switches back to pressure mode
+        if self.config & MODE_ALTIMETER != 0:
+            self.config(self.config ^ MODE_ALTIMETER)
+            time.sleep(2)
+        byte_data = []
 
+        byte_data[0] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_MSB)
+        byte_data[1] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_CSB)
+        byte_data[2] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_LSB)
 
-    def convert_altitude_data(self,data):
-        return float((data[0]<<24)|(data[1]<<16)|(data[2]<<8))/ALTITUDE_DIVISOR
-
-
-    def convert_pressure_data(self,data):
-        return float((data[0]<<16)|(data[1]<<8)|(data[2]))/PRESSURE_DIVISOR
-
-    def convert_temperature_data(self,data):
-        return float((data[0]<<8)|(data[1]))/TEMPERATURE_DIVISOR
+        return self.convert_pressure_data(byte_data)
 
     def get_temperature(self):
         byte_data = []
@@ -71,16 +81,5 @@ class Altimeter:
         byte_data[1] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_T_LSB)
 
         return self.convert_temperature_data(byte_data)
-
-    def get_barometric_pressure(self):
-        if self.config & MODE_ALTIMETER:
-            self.config(self.config ^ MODE_ALTIMETER)
-        byte_data = []
-
-        byte_data[0] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_MSB)
-        byte_data[1] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_CSB)
-        byte_data[2] = self.bus.read_byte_data(MPL3115A2_ADDR, OUT_P_LSB)
-
-
 
     
