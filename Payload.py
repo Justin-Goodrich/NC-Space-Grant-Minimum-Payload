d174b947147c912d1bfc86c78bb37edb2c5b00f1
Justin Goodrich <106884609+Justin-Goodrich@users.noreply.github.com>
2023-02-28 13:52:39 -0500

    Delete Payload.py

--- Payload.py
+++
@@ -1,9 +0,0 @@
-import Sensors.Accelerometer as Accelerometer
-
-class Payload:
-    def __init__(self, accelerometer_address) -> None:
-        self.Accelerometer = Accelerometer(accelerometer_address)
-
-
-if __name__ == "__main__":
-    pass
