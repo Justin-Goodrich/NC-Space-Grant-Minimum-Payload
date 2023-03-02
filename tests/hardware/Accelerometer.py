e5d574a0a165eac283c5cf21d89b48d1fb946415
Justin Goodrich <106884609+Justin-Goodrich@users.noreply.github.com>
2023-03-02 15:40:41 -0500

    Create Accelerometer.py

---
+++ tests/hardware/Accelerometer.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import json
+import time
+import os
+from sensor.Accelerometer import Accelerometer
+
+accelerometer = Accelerometer(1)
+accelerometer.set_active()
+collection_interval = 0.01
+collection_time = 120
+ms = 0
+
+x = []
+y = []
+
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('start data collection\n\n')
+
+t = time.time()
+
+while time.time()-t < collection_time:
+    accelerometer.get_acceleration()
+    x.append(ms)
+    y.append(temp)
+    ms+=1
+    time.sleep(collection_interval)
+
+
+def prompt(directory):
+    i = input()
+    if i in os.listdir(directory):
+        print('file exists, please enter new name')
+        prompt()
+    return i
+
+outputdir = 'data'
+graphdir = 'graphs'
+print('collection complete, please enter filename for data saving:\ndo not include file extension')
+filename = prompt(outputdir)
+out_file = open(os.path.join(outputdir,filename + '.json'), 'w+')
+
+data = {
+    'x':x,
+    'y':y
+}
+
+json.dump(data, out_file)
+out_file.close()
+
+print('input filename for graph image:')
+graph_filename = prompt(graphdir)
+
+plt.plot(x,y)
+plt.savefig(os.path.join(graphdir,graph_filename) + '.png')
