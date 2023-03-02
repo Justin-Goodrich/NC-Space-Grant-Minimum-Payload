314e2e9cbca427c217a5829576ca988fac019bcd
Justin Goodrich <106884609+Justin-Goodrich@users.noreply.github.com>
2023-03-02 15:38:25 -0500

    added camera test
    added camera test

---
+++ tests/hardware/Camera.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import json
+import time
+import os
+from camera.CameraController import ImageCamera
+
+camera = ImageCamera(0)
+collection_interval = 0.01
+collection_time = 60
+
+
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('start data collection\n\n')
+
+t = time.time()
+
+while time.time()-t < collection_time:
+    camera.capture_frame()
+    time.sleep(collection_interval)
+
+camera.image_collection(1,60,'images')
